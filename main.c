3a807d5e9209944912bfaa530a8d69a8b02b0819
Kevan Hollbach <khollbach@gmail.com>
2023-07-13 16:33:26 -0700

    json parsing hello world

--- main.c
+++ main.c
@@ -1,5 +1,22 @@
 #include <stdio.h>
+#include <string.h>
+
+#include <json-c/json.h>
 
 int main() {
+    char json_string[100] = "{\"1\": 2, \"3\": 4, \"5\": null}";
+    // char json_string[100] = "1";
+
+    struct json_tokener *tok = json_tokener_new();
+    enum json_tokener_error jerr;
+    json_object *jobj;
+
+    do {
+        jobj = json_tokener_parse_ex(tok, json_string, strlen(json_string) + 1);
+        jerr = json_tokener_get_error(tok);
+    } while (jerr == json_tokener_continue);
+
+    printf("jobj: %p\n", jobj);
+
     printf("hello world\n");
 }
