cf4c48794c9f82f9172c6b6f35830e2f4df573ee
Ravi Dayabhai <r.i.dayabhai@gmail.com>
2023-07-17 17:50:02 -0700

    Get Maelstrom to read results from STDOUT

--- main.c
+++ main.c
@@ -5,6 +5,9 @@
 #include <json-c/json.h>
 
 json_object* init_reply(json_object* init_msg);
+json_object* echo_reply(json_object* echo_msg);
+
+uint64_t REPLY_MSG_ID = 0;
 
 // ISSUE: Possible memory leaks from not freeing json objects?
 int main()
@@ -13,23 +16,27 @@ int main()
     size_t input_len = 0;
     ssize_t input_bytes_read;
 
-    while ((input_bytes_read = getline(&input_line, &input_len, stdin)) != -1)
+    while ((REPLY_MSG_ID == 0) &&
+           (input_bytes_read = getline(&input_line, &input_len, stdin)) != -1)
     {
         char* json_string = input_line;
         json_object* init_msg = json_tokener_parse(json_string);
         json_object* init_msg_reply = init_reply(init_msg);
         printf("%s\n", json_object_to_json_string(init_msg_reply));
+        fflush(stdout);
         json_object_put(init_msg);
         json_object_put(init_msg_reply);
     }
 
-    while ((input_bytes_read = getline(&input_line, &input_len, stdin)) != -1)
+    while ((input_bytes_read = getline(&input_line, &input_len, stdin)) != 1)
     {
         char* json_string = input_line;
-        json_object* jobj = json_tokener_parse(json_string);
-        const char* parsed_string = json_object_to_json_string(jobj);
-        printf("jobj: %s\n", parsed_string);
-        json_object_put(jobj);
+        json_object* echo_msg = json_tokener_parse(json_string);
+        json_object* echo_msg_reply = echo_reply(echo_msg);
+        printf("%s\n", json_object_to_json_string(echo_msg_reply));
+        fflush(stdout);
+        json_object_put(echo_msg);
+        json_object_put(echo_msg_reply);
     }
     free(input_line);
 }
@@ -38,7 +45,7 @@ int main()
 json_object* init_reply(json_object* init_msg)
 {
     // Create `msg_id`, `dest`, `src`, `in_reply_to` fields
-    json_object* reply_msg_id = json_object_new_int64(1);
+    json_object* reply_msg_id = json_object_new_int64(++REPLY_MSG_ID);
     json_object* reply_dest = json_object_object_get(init_msg, "src");
     json_object* reply_src = json_object_object_get(init_msg, "dest");
     json_object* reply_in_reply_to = json_object_object_get(
@@ -51,13 +58,16 @@ json_object* init_reply(json_object* init_msg)
 
     // Create `body` field
     json_object* reply_body = json_object_new_object();
-    json_object_object_add(reply_body, "in_reply_to", reply_in_reply_to);
-    json_object_object_add(reply_body, "msg_id", reply_msg_id);
     json_object_object_add(reply_body, "type",
                            json_object_new_string("init_ok"));
+    json_object_object_add(reply_body, "msg_id", reply_msg_id);
+    json_object_object_add(reply_body, "in_reply_to", reply_in_reply_to);
 
     // Add `body` to `reply` object
     json_object_object_add(reply, "body", reply_body);
 
     return reply;
 }
+
+// TODO: Need to implement
+json_object* echo_reply(json_object* echo_msg) { return echo_msg; }
