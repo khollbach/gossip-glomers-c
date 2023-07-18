d3f2e040666e1a24e17a746e4103a211ebc75d77
Ravi Dayabhai <r.i.dayabhai@gmail.com>
2023-07-17 19:07:27 -0700

    Implement `echo_reply`; pass Challenge 1 tests

--- main.c
+++ main.c
@@ -19,8 +19,7 @@ int main()
     while ((REPLY_MSG_ID == 0) &&
            (input_bytes_read = getline(&input_line, &input_len, stdin)) != -1)
     {
-        char* json_string = input_line;
-        json_object* init_msg = json_tokener_parse(json_string);
+        json_object* init_msg = json_tokener_parse(input_line);
         json_object* init_msg_reply = init_reply(init_msg);
         printf("%s\n", json_object_to_json_string(init_msg_reply));
         fflush(stdout);
@@ -30,8 +29,7 @@ int main()
 
     while ((input_bytes_read = getline(&input_line, &input_len, stdin)) != 1)
     {
-        char* json_string = input_line;
-        json_object* echo_msg = json_tokener_parse(json_string);
+        json_object* echo_msg = json_tokener_parse(input_line);
         json_object* echo_msg_reply = echo_reply(echo_msg);
         printf("%s\n", json_object_to_json_string(echo_msg_reply));
         fflush(stdout);
@@ -45,7 +43,7 @@ int main()
 json_object* init_reply(json_object* init_msg)
 {
     // Create `msg_id`, `dest`, `src`, `in_reply_to` fields
-    json_object* reply_msg_id = json_object_new_int64(++REPLY_MSG_ID);
+    json_object* reply_msg_id = json_object_new_int64(REPLY_MSG_ID++);
     json_object* reply_dest = json_object_object_get(init_msg, "src");
     json_object* reply_src = json_object_object_get(init_msg, "dest");
     json_object* reply_in_reply_to = json_object_object_get(
@@ -69,5 +67,36 @@ json_object* init_reply(json_object* init_msg)
     return reply;
 }
 
-// TODO: Need to implement
-json_object* echo_reply(json_object* echo_msg) { return echo_msg; }
+json_object* echo_reply(json_object* echo_msg)
+{
+    // Create `msg_id`, `dest`, `src`, `in_reply_to` fields
+    json_object* reply_msg_id = json_object_new_int64(REPLY_MSG_ID++);
+    json_object* reply_dest = json_object_object_get(echo_msg, "src");
+    json_object* reply_src = json_object_object_get(echo_msg, "dest");
+    json_object* reply_in_reply_to = json_object_object_get(
+        json_object_object_get(echo_msg, "body"), "msg_id");
+
+    // Extract `echo` field
+    json_object* reply_echo = json_object_object_get(
+        json_object_object_get(echo_msg, "body"), "echo");
+
+    // Create `reply` and add dest`, `src` fields
+    json_object* reply = json_object_new_object();
+    json_object_object_add(reply, "src", reply_src);
+    json_object_object_add(reply, "dest", reply_dest);
+
+    // Create `body` field
+    json_object* reply_body = json_object_new_object();
+    json_object_object_add(reply_body, "type",
+                           json_object_new_string("echo_ok"));
+    json_object_object_add(reply_body, "msg_id", reply_msg_id);
+    json_object_object_add(reply_body, "in_reply_to", reply_in_reply_to);
+
+    // "Echo" the `echo` field
+    json_object_object_add(reply_body, "echo", reply_echo);
+
+    // Add `body` to `reply` object
+    json_object_object_add(reply, "body", reply_body);
+
+    return reply;
+}
