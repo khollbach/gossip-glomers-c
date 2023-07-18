05642eb1b67237abb283735fac0da31a1a7a8b60
Ravi Dayabhai <r.i.dayabhai@gmail.com>
2023-07-17 22:10:28 -0700

    Narrow down offending `json_object_put`
    * Valgrind still reports memory leak, but calling `json_object_put`
      results in an ref count error :(

--- main.c
+++ main.c
@@ -9,7 +9,6 @@ json_object* echo_reply(json_object* echo_msg);
 
 uint64_t REPLY_MSG_ID = 0;
 
-// ISSUE: Possible memory leaks from not freeing json objects?
 int main()
 {
     char* input_line = NULL;
@@ -23,7 +22,10 @@ int main()
         json_object* init_msg_reply = init_reply(init_msg);
         printf("%s\n", json_object_to_json_string(init_msg_reply));
         fflush(stdout);
-        json_object_put(init_msg);
+        // ISSUE: Memory leak, but calling json_object_put causes SIGABRT
+        // main.out: ./json_object.c:273: json_object_put: Assertion
+        // `jso->_ref_count > 0' failed.
+        /* json_object_put(init_msg); */
         json_object_put(init_msg_reply);
     }
 
@@ -33,14 +35,16 @@ int main()
         json_object* echo_msg_reply = echo_reply(echo_msg);
         printf("%s\n", json_object_to_json_string(echo_msg_reply));
         fflush(stdout);
-        json_object_put(echo_msg);
+        // ISSUE: Memory leak, but calling json_object_put causes SIGABRT
+        // main.out: ./json_object.c:273: json_object_put: Assertion
+        // `jso->_ref_count > 0' failed.
+        /* json_object_put(echo_msg); */
         json_object_put(echo_msg_reply);
     }
 
     free(input_line);
 }
 
-// ISSUE: Possible memory leaks from not freeing json objects?
 json_object* init_reply(json_object* init_msg)
 {
     // Create `msg_id`, `dest`, `src`, `in_reply_to` fields
