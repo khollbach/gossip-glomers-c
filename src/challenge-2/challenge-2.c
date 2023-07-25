d0d70f8887eb34e102a0e437205c3961edb39c10
Ravi Dayabhai <r.i.dayabhai@gmail.com>
2023-07-25 14:32:48 -0700

    [Challenge 2] Implement Follower node message handlers

--- src/challenge-2/challenge-2.c
+++ src/challenge-2/challenge-2.c
@@ -3,42 +3,110 @@
 
 #include "../../lib/collections.h"
 #include "../../lib/util.h"
+#include "json-c/json_object.h"
 
 json_object* init_reply(json_object* init_msg);
 
+void client_request_handler(json_object* client_request);
+void conch_response_handler(json_object* conch_response);
+
+char* LEADER_NODE = "n0";
+
+void client_request_handler(json_object* client_request)
+{
+
+    /* Conch request schema
+    {
+      "src": "n_",
+      "dest": "n0",
+      "body": { "type": "conch_request", "client_id": "c_" }
+    }
+    */
+    json_object* conch_request = json_object_new_object();
+
+    json_object* dest = json_object_new_string(LEADER_NODE);
+    json_object_object_add(conch_request, "dest", dest);
+
+    json_object* src = json_object_object_get(client_request, "dest");
+    json_object_get(src);
+    json_object_object_add(conch_request, "src", src);
+
+    json_object* body = json_object_new_object();
+    json_object_object_add(body, "type",
+                           json_object_new_string("conch_request"));
+    json_object_get(client_request);
+    json_object_object_add(body, "original_client_request", client_request);
+    json_object_object_add(conch_request, "body", body);
+
+    msg_send(conch_request);
+}
+
+void conch_response_handler(json_object* conch_response)
+{
+    /* Conch response schema
+    {
+      "src": "n0",
+      "dest": "n_",
+      "body": { "msg_id": _, "type": "conch_response", "conch_value": _,
+    "original_client_request": _ }
+    }
+    */
+
+    json_object* conch_response_body =
+        json_object_object_get(conch_response, "body");
+    json_object* client_response = generic_reply(
+        json_object_object_get(conch_response_body, "original_client_request"));
+
+    // id set to current conch_value
+    json_object* conch_value =
+        json_object_object_get(conch_response_body, "conch_value");
+    json_object_get(conch_value);
+    json_object_object_add(json_object_object_get(client_response, "body"),
+                           "conch value", conch_value);
+
+    // Send response message to client
+    msg_send(client_response);
+
+    // Increment conch_value
+    int64_t new_conch_value = json_object_get_int64(conch_value) + 1;
+
+    /* Conch release schema
+    {
+      "src": "n_",
+      "dest": "n0",
+      "body": { "type": "conch_release", "conch_value": _ }
+    }
+    */
+    json_object* conch_release = json_object_new_object();
+
+    json_object* dest = json_object_new_string(LEADER_NODE);
+    json_object_object_add(conch_release, "dest", dest);
+
+    json_object* src = json_object_object_get(conch_response, "dest");
+    json_object_get(src);
+    json_object_object_add(conch_release, "src", src);
+
+    json_object* conch_release_body = json_object_new_object();
+    json_object_object_add(conch_release_body, "type",
+                           json_object_new_string("conch_rerelease"));
+    json_object_object_add(conch_release_body, "conch_value",
+                           json_object_new_int64(new_conch_value));
+    json_object_object_add(conch_release, "body", conch_release_body);
+
+    // Send release message to Leader
+    msg_send(conch_release);
+}
+
 int main(void)
 {
-    char* input_line = NULL;
-    size_t input_len = 0;
-    ssize_t input_bytes_read;
-
-    if ((input_bytes_read = getline(&input_line, &input_len, stdin)) != -1)
+    json_object* init_msg = msg_recv();
+    if (init_msg == NULL)
     {
-        json_object* init_msg = json_tokener_parse(input_line);
-        json_object* init_msg_reply = init_reply(init_msg);
-        printf("%s\n", json_object_to_json_string(init_msg_reply));
-        fflush(stdout);
-        json_object_put(init_msg_reply);
-        json_object_put(init_msg);
+        fprintf(stderr, "expected init message, got EOF");
+        exit(EXIT_FAILURE);
     }
-
-    // NOTE: This is purely for testing; will be removed in final version
-    Queue* queue = queue_init(10);
-    for (int i = 0; i < 10; i++)
-    {
-        int* data = malloc(sizeof(int));
-        *data = i;
-        queue_enqueue(queue, data);
-    }
-    for (int i = 0; i < 10; i++)
-    {
-        int* data = queue_dequeue(queue);
-        printf("%d\n", *data);
-        free(data);
-    }
-    queue_free(queue);
-
-    free(input_line);
+    msg_send(init_reply(init_msg));
+    json_object_put(init_msg);
 }
 
 json_object* init_reply(json_object* init_msg)
