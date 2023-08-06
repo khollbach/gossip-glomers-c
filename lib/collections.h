bbdf4ef7db0bd5d882a03a493b517cef8c4f621e
Ravi Dayabhai <r.i.dayabhai@gmail.com>
2023-08-06 15:25:57 -0700

    [NOREF] Implement polymorphic dynamic array

--- lib/collections.h
+++ lib/collections.h
@@ -6,6 +6,7 @@
 
 typedef struct Queue Queue;
 typedef struct Conch Conch;
+typedef struct List List;
 
 Queue* queue_init(size_t size);
 void queue_enqueue(Queue* queue, void* data);
@@ -19,3 +20,11 @@ void conch_checkin(Conch* conch, int64_t new_conch_value);
 int64_t conch_checkout(Conch* conch);
 bool conch_is_available(Conch* conch);
 void conch_free(Conch* conch);
+
+List* list_init(void);
+void list_append(List* list, void* data);
+void* list_pop(List* list);
+void* list_get_at(List* list, size_t index);
+void list_set_at(List* list, size_t index, void* data);
+size_t list_length(List* list);
+void list_free(List* list);
