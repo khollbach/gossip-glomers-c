ee4f0cc88330eaa821e786651ba8c99d4c346875
Samuel Selleck <sellecksamuel@gmail.com>
2023-09-13 12:06:26 -0700

    started getline_with_timeout

--- lib/util.c
+++ lib/util.c
@@ -21,28 +21,60 @@ void msg_send(json_object* msg)
     json_object_put(msg);
 }
 
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/select.h>
+#include <stdio.h>
+#include <unistd.h>
+
+ssize_t getline_with_timeout(
+    char** input_line,
+    size_t* input_len,
+    struct timeval timeout,
+) {
+    fd_set set;
+    FD_ZERO(&set); /* clear the set */
+    FD_SET(STDIN_FILENO, &set); /* add our file descriptor to the set */
+    int buffer_state;
+
+    buffer_state = select(1, &set, NULL, NULL, &timeout);
+
+
+    if(buffer_state == -1) {
+        fprintf(stderr, "Error: select in getline_with_timeout failed\n");
+        exit(EXIT_FAILURE);
+    } else if(buffer_state == 0) {
+        return -1;
+    } else {
+        errno = 0;
+        ssize_t input_read = getline(input_line, input_len, stdin);
+        if (input_read == -1)
+        {
+            // End of stdin; return NULL.
+            if (errno == 0)
+            {
+                free(input_line);
+                return NULL;
+            }
+
+            // IO error.
+            perror("getline");
+            free(input_line);
+            exit(EXIT_FAILURE);
+        }
+
+    }
+}
+
 json_object* msg_recv()
 {
     char* input_line = NULL;
     size_t input_len = 0;
 
     errno = 0;
-    ssize_t input_bytes_read = getline(&input_line, &input_len, stdin);
-    if (input_bytes_read == -1)
-    {
-        // End of stdin; return NULL.
-        if (errno == 0)
-        {
-            free(input_line);
-            return NULL;
-        }
-
-        // IO error.
-        perror("getline");
-        free(input_line);
-        exit(EXIT_FAILURE);
-    }
-
+    ssize_t input_bytes_read = getline_with_timeout(&input_line, &input_len);
     json_object* msg = json_tokener_parse(input_line);
     if (msg == NULL)
     {
